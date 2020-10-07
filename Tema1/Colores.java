7aee9ce20c7e51139c65f61498a3d17dc456d010
Jesús Gutiérrez <jgutiramirez92@gmail.com>
2020-10-07 19:13:30 +0200

    Actualizada la carpeta de practicas-java con tema 1

---
+++ Tema1/Colores.java
@@ -0,0 +1,11 @@
+public class Colores { 
+public static void main (String [] args) {
+System.out.println("\033[33m mandarina");
+System.out.println("\033[32m hierba");
+System.out.println("\033[31m ttomate");
+System.out.println("\033[37m sabanas");
+System.out.println("\033[36m  cielo");
+System.out.println("\033[35m nazareno");
+System.out.println("\033[34m mar");
+}
+}
---
+++ Tema1/caracteresespeciales.java
@@ -0,0 +1,5 @@
+public class caracteresespeciales{
+	public static void main (String [] args){
+	System.out.println("Hola mundo \u266A");
+	 }
+	}
