7a05291ab4eac6f7fbf0316a80ab4cdbe09a465d
Jesús Gutiérrez <jgutiramirez92@gmail.com>
2020-10-10 18:50:30 +0200

    Actualizada carpeta practicas java tema 1 hasta la práctica 4

---
+++ Tema1/Ejercicios final tema/ejercicio2 (copia).java
@@ -0,0 +1,9 @@
+/*Modifica el programa anterior para que además se muestre tu dirección y tu número de teléfono. Asegurate de que los datos se muestran en lineas separadas*/
+public class ejercicio2{
+public static void main (String [] args){
+System.out.println("NOMBRE: Jesús María Gutiérrez Ramírez·");
+System.out.println("DIRECCION: C/Almería 37");
+System.out.print("TLFN: 617710957");
+
+}
+}
---
+++ Tema1/Ejercicios final tema/ejercicio4 (copia).java
@@ -0,0 +1,14 @@
+public class ejercicio4{
+public static void main (String [] args){
+System.out.println("Lunes  Martes   Miércoles  Jueves  Viernes");
+System.out.println("------------------------------------------");
+System.out.println(  "EED    PRO       PRO       PRO      FOL");
+System.out.println(  "LM     PRO       PRO       LM       FOL");
+System.out.println(  "LM     EED       PRO       LM       FOL");
+System.out.println(  "---------------------------------------");
+System.out.println(  "PRO    EED       PRO       SINF    SINF");
+System.out.println(  "PRO    BBDD      PRO       SINF    SINF");
+System.out.println(  "BBDD   BBDD      PRO       SINF    SINF");
+
+}
+}
