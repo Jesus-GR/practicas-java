efeade41382113c8ea0ef51325133350ac7e4de0
Jesús Gutiérrez <jgutiramirez92@gmail.com>
2020-10-12 18:25:54 +0200

    registro de cambios

---
+++ Tema1/Ejercicios final tema/ejercicio5.java
@@ -0,0 +1,15 @@
+public class ejercicio5{
+public static void main (String [] args){
+String name = "\033[30M EED"
+System.out.println("Lunes  Martes   Miércoles  Jueves  Viernes");
+System.out.println("------------------------------------------");
+System.out.printF(  " EDD    PRO       PRO       PRO      FOL");
+System.out.println(  "LM     PRO       PRO       LM       FOL");
+System.out.println(  "LM     EED       PRO       LM       FOL");
+System.out.println(  "---------------------------------------");
+System.out.println(  "PRO    EED       PRO       SINF    SINF");
+System.out.println(  "PRO    BBDD      PRO       SINF    SINF");
+System.out.println(  "BBDD   BBDD      PRO       SINF    SINF");
+
+}
+}
