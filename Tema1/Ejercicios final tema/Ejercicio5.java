1acd9a54323a50801894856a86651dc2d1d2efe2
Jesús Gutiérrez <jgutiramirez92@gmail.com>
2020-10-13 00:23:24 +0200

    Actualización código ejercicios

--- Tema1/Ejercicios final tema/Ejercicio5.java
+++ Tema1/Ejercicios final tema/Ejercicio5.java
@@ -1,9 +1,13 @@
-public class ejercicio5{
+public class Ejercicio5{
 public static void main (String [] args){
-String name = "\033[30M EED"
 System.out.println("Lunes  Martes   Miércoles  Jueves  Viernes");
 System.out.println("------------------------------------------");
-System.out.printF(  " EDD    PRO       PRO       PRO      FOL");
+System.out.print(  " \033[30m EDD \033");
+System.out.print(  " \033[90m PRO \033");
+System.out.print(  " \033[93m PRO \033");
+System.out.print(  " \033[37m PRO \033");
+System.out.println(  " \033[30m FOL \033");
+
 System.out.println(  "LM     PRO       PRO       LM       FOL");
 System.out.println(  "LM     EED       PRO       LM       FOL");
 System.out.println(  "---------------------------------------");
