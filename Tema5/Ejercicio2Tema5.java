8b1e215d53bd7b5bf22e398818bd52ef89e07889
Jesus-GR <jgutiramirez92@gmail.com>
2020-11-05 08:16:23 +0100

    Actualizacón tema 5

---
+++ Tema5/Ejercicio2Tema5.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author Jesús Gutiérrez
+ */
+ public class Ejercicio2Tema5{
+    public static void main(String[]args){
+     
+      int numero;
+      numero = 0;
+      while(numero<100){
+        System.out.println(numero);
+        numero+=5;}
+    }
+  }
---
+++ Tema5/Ejercicio3Tema5.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @author Jesús Gutiérrez
+ */
+ public class Ejercicio3Tema5{
+    public static void main(String[]args){
+     
+      int numero;
+      numero = 0;
+      
+      do{
+        System.out.println(numero);
+        numero+=5;
+      } while (numero<100);
+      
+       
+    }
+  }
