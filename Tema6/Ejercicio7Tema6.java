fcdc604cbc3cf1f07288d5fadf38da18b8ca7a86
Jesus-GR <jgutiramirez92@gmail.com>
2020-11-27 10:55:08 +0100

    Actualización tema 6

--- Tema6/Ejercicio7Tema6.java
+++ Tema6/Ejercicio7Tema6.java
@@ -1,5 +1,5 @@
-
 package tema6;
+
 /**
  *
  * @author Jesús Gutiérrez
@@ -7,7 +7,203 @@ package tema6;
 public class Ejercicio7Tema6 {
 
   public static void main(String[] args) {
-    // TODO code application logic here
+    System.out.println("            ┏━━━━━━━┓");
+    System.out.print("Partido 1: ");
+    switch ((int) (Math.random() * 3) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 2: ");
+    switch ((int) (Math.random() * 3) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 3: ");
+    switch ((int) (Math.random() * 3) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 4: ");
+    switch ((int) (Math.random() * 3) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 5: ");
+    switch ((int) (Math.random() * 3) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 6: ");
+    switch ((int) (Math.random() * 3) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 7: ");
+    switch ((int) (Math.random() * 3) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 8: ");
+    switch ((int) (Math.random() * 3) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 9: ");
+    switch ((int) (Math.random() * 3) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 10: ");
+    switch ((int) (Math.random() * 3) + 1) {
+      case 1:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print("┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 3:
+        System.out.print("┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 11: ");
+    switch ((int) (Math.random() * 3) + 1) {
+      case 1:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print("┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 3:
+        System.out.print("┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 12: ");
+    switch ((int) (Math.random() * 3) + 1) {
+      case 1:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print("┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 3:
+        System.out.print("┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 13: ");
+    switch ((int) (Math.random() * 3) + 1) {
+      case 1:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print("┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 3:
+        System.out.print("┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 14: ");
+    switch ((int) (Math.random() * 3) + 1) {
+      case 1:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print("┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 3:
+        System.out.print("┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┗━━━━━━━┛");
   }
-  
 }
---
+++ Tema6/Ejercicio8Tema6.java
@@ -0,0 +1,335 @@
+package tema6;
+
+/**
+ *
+ * @author Jesús Gutiérrez
+ */
+public class Ejercicio8Tema6 {
+
+  public static void main(String[] args) {
+    System.out.println("            ┏━━━━━━━┓");
+    System.out.print("Partido 1: ");
+    switch ((int) (Math.random() * 6) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 4:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 5:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 6:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 2: ");
+    switch ((int) (Math.random() * 6) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 4:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 5:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 6:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 3: ");
+     switch ((int) (Math.random() * 6) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 4:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 5:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 6:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 4: ");
+     switch ((int) (Math.random() * 6) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 4:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 5:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 6:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 5: ");
+    switch ((int) (Math.random() * 6) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 4:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 5:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 6:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 6: ");
+    switch ((int) (Math.random() * 6) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 4:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 5:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 6:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 7: ");
+    switch ((int) (Math.random() * 6) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 4:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 5:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 6:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 8: ");
+     switch ((int) (Math.random() * 6) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 4:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 5:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 6:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 9: ");
+    switch ((int) (Math.random() * 6) + 1) {
+      case 1:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 3:
+        System.out.print(" ┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 4:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 5:
+        System.out.print(" ┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 6:
+        System.out.print(" ┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 10: ");
+     switch ((int) (Math.random() * 6) + 1) {
+      case 1:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 3:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 4:
+        System.out.print("┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 5:
+        System.out.print("┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 6:
+        System.out.print("┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 11: ");
+     switch ((int) (Math.random() * 6) + 1) {
+      case 1:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 3:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 4:
+        System.out.print("┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 5:
+        System.out.print("┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 6:
+        System.out.print("┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 12: ");
+     switch ((int) (Math.random() * 6) + 1) {
+      case 1:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 3:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 4:
+        System.out.print("┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 5:
+        System.out.print("┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 6:
+        System.out.print("┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 13: ");
+    switch ((int) (Math.random() * 6) + 1) {
+      case 1:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 3:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 4:
+        System.out.print("┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 5:
+        System.out.print("┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 6:
+        System.out.print("┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┣━━━━━━━┫");
+    System.out.print("Partido 14: ");
+    switch ((int) (Math.random() * 6) + 1) {
+      case 1:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 2:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 3:
+        System.out.print("┃\033[031m1\033[39;49m  x  2┃");
+        break;
+      case 4:
+        System.out.print("┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 5:
+        System.out.print("┃1  \033[031mX\033[39;49m  2┃");
+        break;
+      case 6:
+        System.out.print("┃1  x  \033[031m2\033[39;49m┃");
+      default:
+    }
+    System.out.println("");
+    System.out.println("            ┗━━━━━━━┛");
+  }
+}
