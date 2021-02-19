b9d8c4b1ae9754cb5bfff9559352555d4cf14cd1
Jesus-GR <jgutiramirez92@gmail.com>
2021-02-19 17:18:38 +0100

    Tema9

---
+++ Tema9/src/caballo/Caballo.java
@@ -0,0 +1,67 @@
+package caballo;
+
+
+	public class Caballo{
+		private String raza;
+		private String color;
+		private double pesoEnKilos;		
+		private int alturaEnCm;
+		
+		// constructor completo
+		public Caballo(String raza, String color, double pesoEnKilos, int alturaEnCm) {
+			super();
+			this.raza = raza;
+			this.color = color;
+			this.pesoEnKilos = pesoEnKilos;
+			this.alturaEnCm = alturaEnCm;
+		}
+		// constructor vacio
+		public Caballo() {
+			
+		}
+		// metodo corre
+		public void come(double kilos, String comida) {
+			if(comida .equals("heno")) { 
+				this.pesoEnKilos += kilos;
+			}else if( comida .equals("pizza")) {
+				this.pesoEnKilos += kilos + (kilos/2);
+				if(pesoEnKilos < 100){
+					System.out.println("Dame mas de comer, que tengo hambre");
+				}else if(pesoEnKilos >= 100 && pesoEnKilos <1000) {
+					System.out.println("Bueno quizas pueda comer un poco mas");
+				}else {
+					System.out.println("Estoy demasiado lleno, voy a vomitar");
+					this.pesoEnKilos = 50;
+				}
+				
+			}else  {
+				System.out.println("No se que te has creido que soy pero solo puedo comer heno o pizza");
+			}
+			
+		}
+		
+		public void corre(double km) {
+			this.pesoEnKilos-=km;
+		}
+
+		
+//Metodo getter
+		public double getPesoEnKilos() {
+			return pesoEnKilos;
+		}
+
+// Metodo setter
+		public void setPesoEnKilos(double pesoEnKilos) {
+			this.pesoEnKilos = pesoEnKilos;
+		}
+
+		
+		
+
+		
+		
+		
+		
+	}
+	
+
---
+++ Tema9/src/caballo/mainCaballo.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package caballo;
+
+/**
+ * @author Mugre
+ *
+ */
+import java.util.Scanner;
+
+public class mainCaballo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		Caballo rocinante = new Caballo();
+		int cantidadKilos = 0;
+		boolean estaJugando = false;
+		double kilosQueCome = 0;
+		String tipoComida = "";
+		rocinante.setPesoEnKilos(300);
+		System.out.println("Hola, soy Rocinante y peso " + (int) rocinante.getPesoEnKilos() + " kilos");
+		while (!estaJugando) {
+			System.out.print("Que quieres que haga?(1-correr 2-comer 3-Dormir): ");
+			int eleccion = Integer.parseInt(s.nextLine());
+			if (eleccion == 1) {
+				System.out.print("Cuantos kms quieres que corra?: ");
+				double km = Double.parseDouble(s.nextLine());
+				rocinante.corre(km);
+				System.out.println("Ahora mi peso es: " + rocinante.getPesoEnKilos());
+			} else if (eleccion == 2) {
+				System.out.print("Que cantidad de comida quieres darle?: ");
+				kilosQueCome = Double.parseDouble(s.nextLine());
+				System.out.print("Que tipo de comida quieres darle?: ");
+				tipoComida = s.nextLine();
+				rocinante.come(kilosQueCome, tipoComida);
+				System.out.println("Ahora mi peso es: " + rocinante.getPesoEnKilos());
+			}else if(eleccion == 3) {
+				estaJugando = true;
+				System.out.print("Me voy a dormir. Adios ");
+			}
+
+		}
+
+	}
+
+}
